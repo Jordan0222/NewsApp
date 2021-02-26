05b0da99e7f1c1614581d94b361c0f8adc126c49
Jordan0222 <75157592+Jordan0222@users.noreply.github.com>
2021-02-27 00:47:15 +0800

    Initial commit and Navigation Fragment

---
+++ app/src/main/java/com/jordan/newsapp/NewsActivity.kt
@@ -0,0 +1,28 @@
+package com.jordan.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.jordan.newsapp.databinding.ActivityNewsBinding
+
+class NewsActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
